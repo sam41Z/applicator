1f35dd07f6672d3560b3e3b58e46f65f9e71a5e3
Samuel Zweifel <zweifelsa+github@gmail.com>
2024-02-24 22:19:33 +0100

    Tweak application status

--- frontend/src/applications/ApplicationStatus.js
+++ frontend/src/applications/ApplicationStatus.js
@@ -1,77 +1,59 @@
 import {Link} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {EnvelopeFill, EnvelopePlusFill, Pencil, PencilFill, SendFill, SendXFill} from "react-bootstrap-icons";
+import {Dropdown, DropdownItem, DropdownMenu, DropdownToggle} from "reactstrap";
+import {getApplicationByJobId, putApplication} from "./ApplicationAPI";
+import {states, statusColor, statusIcon, statusText} from "./ApplicationStatusUtils";
 
 export default function ApplicationStatus({jobId}) {
-    const url = "http://localhost:3000/applications?jobId=" + jobId;
-    const states = ["DRAFT", "APPLIED", "INTERVIEWING", "OFFERED", "REJECTED", "DECLINED", "ACCEPTED"];
     const [application, setApplication] = useState(undefined);
     const [isLoading, setIsLoading] = useState(true);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-    const fetchApplication = () => {
-        return fetch(url)
-            .then((res) => res.json())
+    useEffect(() => {
+        getApplicationByJobId(jobId)
             .then((d) => {
                 if (d.length > 0) {
                     setApplication(d[0]);
                 }
                 setIsLoading(false);
             });
-    };
+    }, []);
 
-    const saveApplication = (newApplication) => {
-        const requestOptions = {
-            method: "PUT",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify(newApplication)
-        };
-        fetch("http://localhost:3000/applications/" + application.id, requestOptions)
-            .then((res) => res.json())
+    const handleStatusChange = (newStatus) => {
+        const newApplication = {...application};
+        newApplication.status = newStatus;
+        putApplication(newApplication)
             .then((d) => setApplication(d));
     };
 
-    useEffect(() => {
-        fetchApplication();
-    }, []);
-    const bagdeColor = (status) => {
-        switch (status) {
-            case "ACCEPTED":
-                return "text-bg-success";
-            case "DECLINED":
-            case "REJECTED":
-                return "text-bg-danger";
-            default:
-                return "text-bg-warning";
-        }
-    };
-
-    const options = states.map(status => {
-        return <option key={status} value={status}>{status}</option>;
+    const dropDownItems = states.map(status => {
+        return (
+            <DropdownItem className={"text-" + statusColor(status)}
+                          onClick={() => handleStatusChange(status)}>
+                {statusIcon(status)} {statusText(status)}
+            </DropdownItem>
+        );
     });
 
-    const onStatusChange = (newStatus) => {
-        const newApplication = {...application};
-        newApplication.status = newStatus;
-        setApplication(newApplication);
-        saveApplication(newApplication);
-    };
-    if (isLoading) {
-        return (<div></div>);
-    } else if (application) {
-        return (
-            <div className="col-6">
-                <select
-                    className={"form-select " + bagdeColor(application.status)}
-                    value={application.status}
-                    onChange={e => onStatusChange(e.target.value)}
-                >
-                    {options}
-                </select>
-            </div>
-        );
-    } else {
-        return (
-            <Link to={"/jobs/" + jobId + "/apply"} className="btn btn-primary col-6"><SendFill/> Apply</Link>
-        );
-    }
+    return (
+        <div>
+            {!isLoading && application &&
+                <Dropdown isOpen={isDropdownOpen} toggle={() => setIsDropdownOpen(!isDropdownOpen)}>
+                    <DropdownToggle color={statusColor(application.status)} className="d-flex w-75">
+                        <div>{statusIcon(application.status)}</div>
+                        <div className="flex-fill text-start ms-3">{statusText(application.status)}</div>
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        {dropDownItems}
+                    </DropdownMenu>
+                </Dropdown>}
+
+            {!isLoading && !application &&
+                <Link to={"/jobs/" + jobId + "/apply"} className="btn btn-primary d-flex w-75">
+                    <div><SendFill/></div>
+                    <div className="flex-fill text-start ms-3">Apply</div>
+                </Link>}
+        </div>
+    );
 }
